41c8d0fbff8d302282340d0e6b69c2364f2ad5c2
Bernardo Abaurre <emerickabaurreb@gmail.com>
2022-05-05 16:04:42 -0300

    feito até questão 14

--- pset2/MariaDBscript.sql
+++ pset2/MariaDBscript.sql
@@ -64,10 +64,72 @@ where selec.nome_dependente is null;
 -- Questão 8
 select
 concat(f.primeiro_nome,' ',nome_meio,'. ',ultimo_nome) as nome_funcionario,
-trabalha_em.*
+trabalha_em.numero_projeto,horas,numero_departamento
 from funcionario as f
-inner join trabalha_em on f.cpf = trabalha_em.cpf_funcionario;
+inner join trabalha_em on f.cpf = trabalha_em.cpf_funcionario
+order by numero_departamento;
 
+-- Questão 9
+select
+projeto.nome_projeto,
+ph.horas_totais,
+departamento.nome_departamento
+from((
+(select numero_projeto,sum(horas) as horas_totais from trabalha_em group by numero_projeto) as ph
+inner join projeto on projeto.numero_projeto = ph.numero_projeto)
+inner join departamento on departamento.numero_departamento = projeto.numero_departamento);
+
+-- Questão 10
+select
+avg(salario) as media_salarial,
+te.numero_departamento
+from funcionario
+inner join (select distinct cpf_funcionario,numero_departamento from trabalha_em) as te on funcionario.cpf = te.cpf_funcionario
+group by numero_departamento;
+
+-- Questão 11
+select
+concat(f.primeiro_nome,' ',nome_meio,'. ',ultimo_nome) as nome_funcionario,
+projeto.nome_projeto,
+(trabalha_em.horas * 50) as valor
+from ((funcionario as f
+inner join trabalha_em
+on f.cpf = trabalha_em.cpf_funcionario)
+inner join projeto on projeto.numero_projeto = trabalha_em.numero_projeto);
+
+-- Questão 12
+select
+concat(f.primeiro_nome,' ',nome_meio,'. ',ultimo_nome) as nome_funcionario,
+te.horas,
+projeto.nome_projeto,
+d.nome_departamento
+from(((
+funcionario as f
+inner join (select * from trabalha_em where horas = 0) as te 
+on f.cpf = te.cpf_funcionario)
+inner join projeto on projeto.numero_projeto = te.numero_projeto)
+inner join departamento as d on d.numero_departamento = te.numero_departamento);
+
+-- Questão 13
+select
+concat(f.primeiro_nome,' ',nome_meio,'. ',ultimo_nome) as nome,
+f.sexo,
+YEAR(CURRENT_TIMESTAMP())-YEAR(f.data_nascimento) as idade
+from funcionario as f
+union
+select
+d.nome_dependente as nome,
+d.sexo,
+YEAR(CURRENT_TIMESTAMP())-YEAR(d.data_nascimento) as idade
+from dependente as d
+order by idade desc;
+
+-- Questão 14
+select count(tab.cpf_funcionario) as qtd_funcionarios ,numero_departamento
+from (select distinct cpf_funcionario,numero_departamento from trabalha_em) as tab
+group by numero_departamento;
+
+-- Questão 15
 
 
 
